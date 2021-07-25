bebf7133a50b7abe2d3bf8d77c0ae550cdf67a80
ashu1450 <83029388+ashu1450@users.noreply.github.com>
2021-07-26 01:05:35 +0545

    Foods

---
+++ script.js
@@ -0,0 +1,29 @@
+document.querySelector(".menu").addEventListener("click",()=>{
+  document.querySelectorAll(".target").forEach((item)=>{
+    item.classList.toggle("change")
+  });
+});
+
+
+var icons = document.querySelectorAll(".section-1-icons i");
+
+var i=1;
+
+setInterval(()=>{
+    i++;
+
+    var icon = document.querySelector(".section-1-icons .change");
+
+    icon.classList.remove("change");
+
+    if (i >icons.length) {
+        icons[0].classList.add("change");
+        i=1;
+    }
+    else{
+        icon.nextElementSibling.classList.add("change");
+
+    }
+
+
+},3000);
